2662ed9a2fd3de3a18ab6452731b35944433e353
Davipcrs <davipernalonga30@gmail.com>
2023-07-30 23:58:32 -0300

    recreating the alarm view layout

--- lib/view/alarm_view.dart
+++ lib/view/alarm_view.dart
@@ -22,24 +22,32 @@ class _AlarmViewState extends State<AlarmView> {
             child: Column(
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("Nome do alarme"),
-                    Text("Start"),
-                    Text("Hora"),
+                    Text(
+                      "hora",
+                      style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.width * 0.07),
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 16, top: 12),
+                          child: Switch(
+                            value: test,
+                            onChanged: (value) => print("changed"),
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
                 Row(
                   children: [
-                    Text("Desc"),
-                    Text("end"),
-                    Text("Hora"),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Switch(
-                      value: test,
-                      onChanged: (value) => print("changed"),
+                    Text(
+                      "Desc",
+                      style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.width * 0.05),
                     ),
                   ],
                 ),
